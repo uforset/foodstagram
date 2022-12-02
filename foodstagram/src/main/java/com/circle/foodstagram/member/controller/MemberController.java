e6004721107ad5ded33ea0daacb68272d0f6ef95
dowonseon <icaca159@naver.com>
2022-12-02 14:53:57 +0900

    최적화 및 카카오 연결 진행중

--- foodstagram/src/main/java/com/circle/foodstagram/member/controller/MemberController.java
+++ foodstagram/src/main/java/com/circle/foodstagram/member/controller/MemberController.java
@@ -80,36 +80,6 @@ public class MemberController {
 	
 	
 	
-	// 회원정보 변경을 위한 본인 인증 페이지로 이동
-		@RequestMapping("moveup.do")
-		public String checkselfPage(
-				@RequestParam("userid") String userid, 
-				Model model) {
-			Member member = memberService.selectMember(userid);
-			if(member != null) {
-				model.addAttribute("member", member);
-				return "member/checkself";
-			}else {
-				model.addAttribute("message", userid + " : 회원 조회 실패!");
-				return "common/error";
-			}
-		}
-		
-		// 본인 인증 후 회원 정보 수정 페이지로 이동
-		@RequestMapping("updatePage.do")
-		public String moveUpdatePage(
-				@RequestParam("userid") String userid, 
-				Model model) {
-			Member member = memberService.selectMember(userid);
-			if(member != null) {
-				model.addAttribute("member", member);
-				return "member/updatePage";
-			}else {
-				model.addAttribute("message", userid + " : 회원 조회 실패!");
-				return "common/error";
-			}
-		}
-	
 	
 	// -------------------------------------------------------------
 	
@@ -256,59 +226,6 @@ public class MemberController {
 			String result = memberService.find_id(member);
 		return result;
 		}
-
-		//회원 정보 수정용 : 수정 성공시 myinfoPage.jsp 로 이동함
-		@RequestMapping(value="mupdate.do", method=RequestMethod.POST)
-		public String memberUpdateMethod(Member member, Model model, 
-				@RequestParam("origin_userpassword") String originUserpassword, HttpServletRequest request) {
-			logger.info("mupdate.do : " + member);
-			logger.info("origin_userpassword : " + originUserpassword);
-			
-			//새로운 암호가 전송이 왔다면, 패스워드 암호화 처리함
-			String userpwd = member.getUserpwd().trim();
-			if(userpwd != null && userpwd.length() > 0) {
-				//기존 암호와 다른 값이면
-				if(!this.bcryptPasswordEncoder.matches(userpwd, originUserpassword)) {
-					//member 에 새로운 패스워드를 암호화해서 기록함
-					member.setUserpwd(this.bcryptPasswordEncoder.encode(userpwd));
-				}
-			}else {
-				//새로운 패스워드 값이 없다면, member 에 원래 패스워드 기록
-				member.setUserpwd(originUserpassword);
-			}
-			
-			logger.info("after : " + member);
-			
-			if(memberService.updateMember(member) > 0) {
-				//수정이 성공했다면, 컨트롤러의 메소드를 직접 호출할 수도 있음
-				//즉, 컨트롤러 안에서 다른 컨트롤러를 실행할 수도 있음
-				//내정보보기 페이지에 수정된 회원정보를 다시 조회해서 내보냄
-				//쿼리스트링 : ?이름=값&이름=값
-				HttpSession session = request.getSession(false);
-				session.invalidate(); //세션 객체를 없앰
-				System.out.println("세션 삭제됨");
-				return "redirect:main.do";
-			}else {
-				model.addAttribute("message", 
-						member.getUserid() + " : 회원 정보 수정 실패!");
-				return "common/error";
-			}
-			
-		}
-		
-		//회원 탈퇴 처리용 : 회원 정보 삭제함
-		//삭제되면 자동 로그아웃함
-		@RequestMapping("mdel.do")
-		public String memberDeleteMethod(
-				@RequestParam("userid") String userid, Model model) {
-			logger.info(userid);
-			if(memberService.deleteMember(userid) > 0) {
-				return "redirect:logout.do";
-			}else {
-				model.addAttribute("message", userid + " : 회원 삭제 실패!");
-				return "common/error";
-			}
-		}
 		
 		// 임시 로그인 후 새로운 비밀번호 설정
 		@RequestMapping(value="resetpwd.do", method=RequestMethod.POST)
@@ -363,93 +280,5 @@ public class MemberController {
 			
 			return viewName;
 		}
-		
-	//로그인 제한/가능 변경 처리용
-	@RequestMapping("loginok.do")
-	public String changeLoginOKMethod(Member member, Model model) {
-		logger.info("loginok.do : " + member.getUserid() + ", " + member.getLoginok());
-		
-		if(memberService.updateLoginOK(member) > 0) {
-			return "redirect:mlist.do";
-		}else {
-			return "member/memberListView";
-		}
-	}
-
-	//회원 검색 처리용 
-	@RequestMapping(value="msearch.do", method=RequestMethod.POST)
-	public String memberSearchMethod(
-			HttpServletRequest request, Model model) {
-		String action = request.getParameter("action");
-		String keyword = request.getParameter("keyword");
-			
-		//서비스 메소드 리턴값 받을 리스트 준비
-		ArrayList<Member> list = null;
-		
-		switch(action) {
-		
-			case "userid": 	list = memberService.selectSearchUserid(keyword);
-					break;
-		
-			case "userloginok": list = memberService.selectSearchLoginOK(keyword);
-					break;
-					
-			case "0" : list = memberService.selectList2(null);
-					break;
-			
-		}
-		
-		if(list.size() > 0) {
-			model.addAttribute("list", list);
-			return "member/memberListView";
-		}else {
-			return "member/memberListView";
-		}
-	}
 	
-	
-	// ------------------------------------------
-	// sns 로그인 콜백 페이지 이동
-	
-	//카카오
-	@RequestMapping(value = "/kcallback.do", produces = "application/json", 
-			method = { RequestMethod.GET, RequestMethod.POST })
-	public String kakaoLogin(
-			@RequestParam("code") String code, 
-			Model model, HttpSession session) throws Exception {
-
-		// 결과값을 node에 담아줌
-		JsonNode node = kakao_loginapi.getAccessToken(code); // accessToken에 사용자의 로그인한 모든 정보가 들어있음
-		JsonNode accessToken = node.get("access_token"); // 사용자의 정보
-		JsonNode userInfo = kakao_loginapi.getKakaoUserInfo(accessToken);
-
-		// 회원가입시 사용		
-		String kid = null;//카카오 로그인 id 값 받아오기
-		// 유저정보 카카오에서 가져오기 Get properties
-		JsonNode properties = userInfo.path("properties");
-		JsonNode kakao_account = userInfo.path("kakao_account");// 값 가져올 수 있지만 사용은 안함		
-		kid = userInfo.path("id").asText();
-		
-		// *값 받아온 것을 회원 정보와 확인해야함*//
-		Member member = new Member();
-		// 아이디 저장되 있는 이름 만들기
-		String checkid = "@K" + kid;// 카카오 아이디 저장 값
-		member.setUserid(checkid);
-		
-		// 간편 로그인 아이디에 해당하는 회원 정보 확인하러가기
-		//Member loginMember = memberService.selectLogin(member);
-		//if(loginMember != null) {//해당하는 아이디가 있으면 로그인 완료
-		if(checkid != null) {  //테스트용 조건임	
-			//카카오 로그인 세션에 저장
-			session.setAttribute("loginMember", member); // 세션 생성			
-			
-			return "common/main";
-			
-		}else {
-			model.addAttribute("message", "카카오 로그인 회원 조회 실패");
-			return "common/error";
-			
-		}
-
-	}// end kakaoLogin()
 }
