30ad8466bad6558ed76243abfef9d5c8fa898ec1
dowonseon <icaca159@naver.com>
2022-12-08 11:31:39 +0900

    카카오, 네이버 컨트롤러 추가

--- foodstagram/src/main/java/com/circle/foodstagram/member/model/vo/KakaoLoginApi.java
+++ foodstagram/src/main/java/com/circle/foodstagram/member/model/vo/KakaoLoginApi.java
@@ -21,78 +21,78 @@ import org.codehaus.jackson.map.ObjectMapper;
 public class KakaoLoginApi {
 	
 	private final static String K_CLIENT_ID = "a6d2a7aa522e1a56f705fac9d21e56e1";
-	private final static String K_REDIRECT_URI = "http://127.0.0.1:8080/foodstagram/main.do";
-	
+	private final static String K_REDIRECT_URI = "http://127.0.0.1:8080/foodstagram/kcallback.do";
+
 	// 카카오로그인 요청시 카카오 로그인 페이지로 이동 url
 	public String getAuthorizationUrl(HttpSession session) {
+
 		String kakaoUrl = "https://kauth.kakao.com/oauth/authorize?" + "client_id=" + K_CLIENT_ID + "&redirect_uri="
 				+ K_REDIRECT_URI + "&response_type=code";
 		return kakaoUrl;
 	}
-	
 	//접근토큰
-		public JsonNode getAccessToken(String autorize_code) {
+	public JsonNode getAccessToken(String autorize_code) {
 
-			final String RequestUrl = "https://kauth.kakao.com/oauth/token";
-			final List<NameValuePair> postParams = new ArrayList<NameValuePair>();
+		final String RequestUrl = "https://kauth.kakao.com/oauth/token";
+		final List<NameValuePair> postParams = new ArrayList<NameValuePair>();
 
-			postParams.add(new BasicNameValuePair("grant_type", "authorization_code"));
-			postParams.add(new BasicNameValuePair("client_id", K_CLIENT_ID));
-			postParams.add(new BasicNameValuePair("redirect_uri", K_REDIRECT_URI));
-			postParams.add(new BasicNameValuePair("code", autorize_code));
+		postParams.add(new BasicNameValuePair("grant_type", "authorization_code"));
+		postParams.add(new BasicNameValuePair("client_id", K_CLIENT_ID));
+		postParams.add(new BasicNameValuePair("redirect_uri", K_REDIRECT_URI));
+		postParams.add(new BasicNameValuePair("code", autorize_code));
 
-			final HttpClient client = HttpClientBuilder.create().build();
-			final HttpPost post = new HttpPost(RequestUrl);
+		final HttpClient client = HttpClientBuilder.create().build();
+		final HttpPost post = new HttpPost(RequestUrl);
 
-			JsonNode returnNode = null;
+		JsonNode returnNode = null;
 
-			try {
+		try {
 
-				post.setEntity(new UrlEncodedFormEntity(postParams));
-				final HttpResponse response = client.execute(post);
-				ObjectMapper mapper = new ObjectMapper();
-				returnNode = mapper.readTree(response.getEntity().getContent());
+			post.setEntity(new UrlEncodedFormEntity(postParams));
+			final HttpResponse response = client.execute(post);
+			ObjectMapper mapper = new ObjectMapper();
+			returnNode = mapper.readTree(response.getEntity().getContent());
 
-			} catch (UnsupportedEncodingException e) {
-				e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
 
-			} catch (ClientProtocolException e) {
-				e.printStackTrace();
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
 
-			} catch (IOException e) {
-				e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
 
-			} finally {
-
-			}
-
-			return returnNode;
+		} finally {
 
 		}
+
+		return returnNode;
+
+	}
+	
+	//사용자 정보 가져오기
+	public static JsonNode getKakaoUserInfo(JsonNode accessToken) {
+		//url 버전 변경됨 v2로
+		final String RequestUrl = "https://kapi.kakao.com/v2/user/me";
+		final HttpClient client = HttpClientBuilder.create().build();
+		final HttpPost post = new HttpPost(RequestUrl);
 		
-		//사용자 정보 가져오기
-		public static JsonNode getKakaoUserInfo(JsonNode accessToken) {
-			//url 버전 변경됨 v2로
-			final String RequestUrl = "https://kapi.kakao.com/v2/user/me";
-			final HttpClient client = HttpClientBuilder.create().build();
-			final HttpPost post = new HttpPost(RequestUrl);
-			
-			// add header
-			//**주의 accessToken 값은 JsonNode형이어야 함, Bearer뒤에 스페이스바 
-			post.addHeader("Authorization", "Bearer " + accessToken);
-			JsonNode returnNode = null;
-			
-			try {
-				final HttpResponse response = client.execute(post); 
-				ObjectMapper mapper = new ObjectMapper();
-				returnNode = mapper.readTree(response.getEntity().getContent());
+		// add header
+		//**주의 accessToken 값은 JsonNode형이어야 함, Bearer뒤에 스페이스바 
+		post.addHeader("Authorization", "Bearer " + accessToken);
+		JsonNode returnNode = null;
+		
+		try {
+			final HttpResponse response = client.execute(post); 
+			ObjectMapper mapper = new ObjectMapper();
+			returnNode = mapper.readTree(response.getEntity().getContent());
 
-			} catch (ClientProtocolException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			} finally { 
-			}
-			return returnNode;
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally { 
 		}
+		return returnNode;
+	}
 }
