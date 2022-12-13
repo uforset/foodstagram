c341c1e2f6324e55f66ba9cd27c5ac1b32d95efb
dowonseon <94886406+dowonseon@users.noreply.github.com>
2022-12-13 09:35:04 +0900

    네이버 REDIRECT_URI 수정

--- foodstagram/src/main/java/com/circle/foodstagram/member/controller/NaverLoginAuth.java
+++ foodstagram/src/main/java/com/circle/foodstagram/member/controller/NaverLoginAuth.java
@@ -30,7 +30,7 @@ public class NaverLoginAuth {
 	// state: 애플리케이션이 생성한 상태 토큰
 	private final static String CLIENT_ID = "Vj4dFHCEvSx5oz95b7Ws";
 	private final static String CLIENT_SECRET = "deq7c5xZYo";
-	private final static String REDIRECT_URI = "http://localhost:8080/foodstagram/naverCallback.do";
+	private final static String REDIRECT_URI = "http://localhost:8080/foodstagram/ncallback.do";
 	private final static String SESSION_STATE = "oauth_state";
 	/* 프로필 조회 API URL */
 	private final static String PROFILE_API_URL = "https://openapi.naver.com/v1/nid/me";
